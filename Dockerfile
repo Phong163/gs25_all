ce8dbdb006fbc3423b8eb05092c42dc3e898a464
Thai Hoang <thai.hoangvan@cxview.ai>
2025-09-30 19:42:59 +0700

    first commit

---
+++ Dockerfile
@@ -0,0 +1,47 @@
+# Sử dụng base image có CUDA Toolkit
+FROM nvidia/cuda:12.2.0-devel-ubuntu22.04
+
+ENV DEBIAN_FRONTEND=noninteractive
+ENV PYTHONUNBUFFERED=1 \
+    PYTHONDONTWRITEBYTECODE=1 \
+    PIP_NO_CACHE_DIR=1
+ENV CUDA_PATH=/usr/local/cuda
+ENV PATH=$CUDA_PATH/bin:$PATH
+ENV LD_LIBRARY_PATH=$CUDA_PATH/lib64:$LD_LIBRARY_PATH
+
+# Cài đặt các gói hệ thống cần thiết
+RUN apt-get update && apt-get install -y --no-install-recommends \
+    python3 \
+    python3-dev \
+    python3-pip \
+    build-essential \
+    cmake \
+    git \
+    libgl1 \
+    libglib2.0-0 \
+    librdkafka-dev \
+    libcudnn8 \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+
+# Cài numpy trước để tránh lỗi khi install deep-person-reid
+RUN pip3 install numpy Cython
+
+
+# Cài requirements chung trước (không chứa deep-person-reid)
+COPY requirements.txt .
+RUN pip3 install -r requirements.txt
+
+# Cài deep-person-reid sau (nó cần numpy đã có sẵn)
+RUN pip3 install git+https://github.com/KaiyangZhou/deep-person-reid.git
+
+# Sao chép mã nguồ
+WORKDIR /app
+COPY . .
+
+RUN apt-get update && apt-get install -y tzdata
+# Thiết lập múi giờ Việt Nam
+ENV TZ=Asia/Ho_Chi_Minh
+RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+# Lệnh mặc định khi chạy container
+CMD ["python3", "your_script.py"]
---
+++ config/params_metric5.py
@@ -0,0 +1,86 @@
+camera_configs = {
+    "vn300":{
+        "box_id":"d2329d74-0219-47d3-ae75-32c12fe95723",
+        "cameras":{
+            0: {
+                "rtsp_url": "rtsp://cxview:gs252525@42.1.117.16:554/Streaming/Channels/401",
+                "zone_1": [
+                    (0.59, 0.36), (0.28, 0.3), (0.18, 0.99), (0.83, 1.0)
+                ],
+                "track_person": {},
+                "current_frame": 0
+            },
+            1: {
+                "rtsp_url": "rtsp://cxview:gs252525@42.1.117.16:554/Streaming/Channels/601",
+                "zone_1": [
+                    (0.47, 0.34), (0.39, 0.36), (0.36, 0.74), (0.53, 0.69)
+                ],
+                "track_person": {},
+                "current_frame": 0
+            },
+            2: {
+                "rtsp_url": "rtsp://cxview:gs252525@42.1.117.16:554/Streaming/Channels/1001",
+                "zone_1": [
+                    (0.64, 0.44), (0.51, 0.41), (0.35, 0.98), (0.6, 0.99)
+                ],
+            
+                "track_person": {},
+                "current_frame": 0
+            },
+            3: {
+                "rtsp_url": "rtsp://cxview:gs252525@42.1.117.16:554/Streaming/Channels/1101",
+                "zone_1": [
+                    (0.6, 0.24), (0.44, 0.24), (0.39, 0.99), (0.7, 0.99)
+                ],
+                "track_person": {},
+                "current_frame": 0
+            },
+            4: {
+                "rtsp_url": "rtsp://cxview:gs252525@42.1.117.16:554/Streaming/Channels/1401",
+                "zone_1": [
+                    (0.77, 0.36), (0.69, 0.35), (0.55, 0.99), (0.78, 0.99)
+                ],
+                "track_person": {},
+                "current_frame": 0
+            },
+        }
+    },
+    "vn316":{
+        "box_id":"d2329d74-0219-47d3-ae75-32c12fe95723",
+        "cameras":{
+            1: {
+                "rtsp_url": "rtsp://cxview:gs252525@115.79.136.220:554/Streaming/Channels/101",
+                "zone_1": [
+                    (0.54, 0.24), (0.42, 0.24), (0.35, 0.91), (0.58, 0.92)
+                ],
+                "track_person": {},
+                "current_frame": 0
+            },
+            3: {
+                "rtsp_url": "rtsp://cxview:gs252525@115.79.136.220:554/Streaming/Channels/301",
+                "zone_1": [
+                    (0.33, 0.36), (0.19, 0.38), (0.18, 0.99), (0.42, 1.0)
+                ],
+                "track_person": {},
+                "current_frame": 0
+            },
+            4: {
+                "rtsp_url": "rtsp://cxview:gs252525@115.79.136.220:554/Streaming/Channels/401",
+                "zone_1": [
+                    (0.86, 0.38), (0.74, 0.36), (0.61, 0.99), (0.86, 0.99)
+                ],
+                "track_person": {},
+                "current_frame": 0
+            },
+            5: {
+                "rtsp_url": "rtsp://cxview:gs252525@115.79.136.220:554/Streaming/Channels/1101",
+                "zone_1": [
+                    (0.74, 0.25), (0.61, 0.25), (0.58, 0.52), (0.78, 0.54)
+                ],
+                "track_person": {},
+                "current_frame": 0
+            },
+        }
+    },
+}
+
